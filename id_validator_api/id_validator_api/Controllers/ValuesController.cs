e33d253abb5fa5d9fd09008f4007b4d58577c828
alootus <ingellootus@gmail.com>
2022-04-04 18:47:15 +0300

    c# api

---
+++ id_validator_api/id_validator_api/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace id_validator_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
